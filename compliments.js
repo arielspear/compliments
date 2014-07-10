8df1686d801b579dea4502d8cd02fd97d83254e9
Ariel Spear <simpleredfox@gmail.com>
2014-07-10 14:23:29 -0700

    initial product

---
+++ compliments.js
@@ -0,0 +1,8 @@
+var complimentArray = [
+"You're smart.", "People like you.", "You are awesome.",
+ "You've got super sweet skills.", "You rock!", "Your files are so well orginized."
+];
+
+var compliment = complimentArray[Math.floor(Math.random()*complimentArray.length)];
+
+console.log(compliment);
